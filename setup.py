b85b0b06e622948bf2d337fb8bd16fc1b3bf65f9
y0gurt-dv <sigirt3@gmail.com>
2022-12-05 14:12:45 +0300

    feat: add long_description to pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 import setuptools
 
+with open('README.md', 'r') as fp:
+    long_description = fp.read()
+
 setuptools.setup(
     name='easy_generate_excel',
-    version="0.0.3",
-    author='Daniil',
+    version="0.0.5",
+    author='Daniil y0gur-dv',
     description='Easy generate excel files',
     packages=['easy_generate_excel'],
     install_requires=[
         'openpyxl',
     ],
     url='https://github.com/y0gurt-dv/Easy-generate-excel',
-    keywords=['excel', 'generators', 'easy']
+    keywords=['excel', 'generators', 'easy'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
